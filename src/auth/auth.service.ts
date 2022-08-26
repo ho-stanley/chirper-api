fe2a129d79f80a13c60663bad84a948c76506edf
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-08-26 12:13:21 +0200

    Update auth module
    AuthService uses password module to compare passwords now.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,25 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
 import { UsersService } from 'src/users/users.service';
+import { PasswordService } from 'src/utils/password/password.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
+    private readonly passwordService: PasswordService,
   ) {}
 
-  async validateUser(username: string, password: string): Promise<any> {
-    const user = await this.usersService.findOne(username);
-    if (user && user.password === password) {
-      const { password, ...result } = user;
-      return result;
+  async validateUser(username: string, password: string): Promise<User | null> {
+    const user = await this.usersService.findOne({ username });
+    if (
+      user &&
+      (await this.passwordService.comparePassword(password, user.password))
+    ) {
+      return user;
     }
     return null;
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { username: user.username, sub: user.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
