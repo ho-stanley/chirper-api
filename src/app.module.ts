e6191c3dda9e9291aaa6f20096a825a82f8e7183
ho-stanley <70745911+ho-stanley@users.noreply.github.com>
2022-08-29 15:44:10 +0200

    Config setup (#4)
    * Update environment variables
    
    - Provided a sample environment file for use.
    - Environment variables are no longer hardcoded and version controlled.
    
    * Add nestjs config to dependency
    
    * Update auth configuration
    
    JWT secret key is no longer hardcoded and is read from environment
    variables instead.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env'],
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
