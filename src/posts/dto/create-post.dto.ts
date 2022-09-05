069d003b62ee0a15ead0d08a80c5bb10fb3a2e51
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-05 21:44:50 +0200

    Add create post DTO

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,1 +1,13 @@
-export class CreatePostDto {}
+import { IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(50)
+  title: string;
+
+  @IsString()
+  @MinLength(1)
+  @MaxLength(250)
+  body: string;
+}
