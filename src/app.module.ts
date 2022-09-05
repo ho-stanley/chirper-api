c2d5c5cae04b999f289ec871cce8e84024aa7d70
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-05 17:12:04 +0200

    Add posts CRUD scaffolding

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
+    PostsModule,
     ConfigModule.forRoot({
       envFilePath: ['.env.development', '.env'],
       isGlobal: true,
