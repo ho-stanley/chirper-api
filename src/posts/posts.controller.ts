1cae34ee40c4dddbfa8fcf2696661f2fc51c48fb
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-09 17:16:19 +0200

    Add CRUD methods to posts route

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -3,21 +3,24 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  UseGuards,
+  Req,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RequestWithUser } from 'src/utils/typings/request-user';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postsService.create(createPostDto);
+  create(@Body() createPostDto: CreatePostDto, @Req() req: RequestWithUser) {
+    return this.postsService.create(createPostDto, req.user);
   }
 
   @Get()
@@ -27,16 +30,12 @@ export class PostsController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.postsService.findOne(+id);
+    return this.postsService.findOne(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postsService.update(+id, updatePostDto);
-  }
-
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.postsService.remove(+id);
+    return this.postsService.remove(id);
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Post } from '@prisma/client';
+import { PrismaService } from 'src/utils/prisma/prisma.service';
+import { JwtData } from 'src/utils/typings/request-jwt';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createPostDto: CreatePostDto, user: JwtData): Promise<Post> {
+    const post = await this.prismaService.post.create({
+      data: {
+        authorId: user.id,
+        authorName: user.username,
+        ...createPostDto,
+      },
+    });
+    return post;
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  findAll(): Promise<Post[]> {
+    return this.prismaService.post.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} post`;
+  async findOne(id: string): Promise<Post | null> {
+    try {
+      const post = await this.prismaService.post.findUnique({
+        where: {
+          id,
+        },
+      });
+      return post;
+    } catch (e: any) {
+      /**
+       * Prisma query will throw an error on malformed ObjectID
+       * and needs to be handled.
+       */
+      throw new BadRequestException();
+    }
   }
 
-  update(id: number, updatePostDto: UpdatePostDto) {
-    return `This action updates a #${id} post`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} post`;
+  async remove(id: string) {
+    return this.prismaService.post.deleteMany({
+      where: {
+        id,
+      },
+    });
   }
 }
