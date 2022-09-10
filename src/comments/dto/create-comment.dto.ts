4147bd9da2eb2e2fdcb9fbede0f856a8809c9088
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-10 16:47:26 +0200

    Add create comment DTO

--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,8 @@
-export class CreateCommentDto {}
+import { IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateCommentDto {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(256)
+  body: string;
+}
