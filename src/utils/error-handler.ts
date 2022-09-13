884f024067cfb10d564b4faaded2963d17ab8d86
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-13 17:07:08 +0200

    feat: add error handler for no resource
    Return an error message if resource could not be found.

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -6,10 +6,12 @@ import {
 
 export function prismaQueryError(e: unknown): never {
   if (e instanceof NotFoundError)
-    throw new BadRequestException('Post not found');
+    throw new BadRequestException('Resource not found');
 
-  if (e instanceof PrismaClientKnownRequestError)
+  if (e instanceof PrismaClientKnownRequestError) {
     if (e.code === 'P2023') throw new BadRequestException('Malformed ID');
+    if (e.code === 'P2025') throw new BadRequestException('Resource not found');
+  }
 
   throw e;
 }
