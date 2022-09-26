f3f60e87b40d60ac4106ca85e4f7c82168598b69
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-27 00:03:08 +0200

    feat: add more validation to create user DTO
    - Increase minimum length on password to 8 characters
    - Added allowed characters whitelist for username

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,18 +1,21 @@
-import { IsString, MaxLength, MinLength } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
+  @Matches(/^\w+$/, {
+    message: 'Only A-Z letters, numbers and underscore are allowed',
+  })
   @MinLength(3)
   @MaxLength(25)
   username: string;
 
   @IsString()
-  @MinLength(6)
+  @MinLength(8)
   @MaxLength(250)
   password: string;
 
   @IsString()
-  @MinLength(6)
+  @MinLength(8)
   @MaxLength(250)
   repeatPassword: string;
 }
