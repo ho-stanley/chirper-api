1bf55bb9e853fb5b3fc547c04a3bb25fb2234631
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2022-09-27 00:09:54 +0200

    refactor: change login route to return http code 200

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Controller, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
 import { RequestJwt } from 'src/utils/typings/request-jwt';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
@@ -8,6 +8,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
+  @HttpCode(200)
   @Post('/login')
   async login(@Req() req: RequestJwt) {
     return this.authService.login(req.user);
