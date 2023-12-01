73672b1c26582360f61a7f172be30ef115ccbd43
Stanley Ho <70745911+ho-stanley@users.noreply.github.com>
2023-12-01 22:32:07 +0100

    chore: add OpenAPI spec to comments controller

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -9,23 +9,33 @@ import {
   UseGuards,
   Req,
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RequestWithUser } from 'src/utils/typings/request-user';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { CommentDto } from './dto/comment.dto';
 
 @Controller()
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
   @UseGuards(JwtAuthGuard)
+  @ApiTags('posts')
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: CommentDto })
   @Post('posts/:postId/comments')
   create(
     @Body() createCommentDto: CreateCommentDto,
     @Param('postId') postId: string,
     @Req() req: RequestWithUser,
-  ) {
+  ): Promise<CommentDto> {
     const { id, username } = req.user;
 
     return this.commentsService.create({
@@ -37,22 +47,29 @@ export class CommentsController {
   }
 
   @Get('posts/:postId/comments')
-  findAllByPostId(@Param('postId') postId: string) {
+  @ApiTags('posts')
+  @ApiOkResponse({ type: [CommentDto] })
+  findAllByPostId(@Param('postId') postId: string): Promise<CommentDto[]> {
     return this.commentsService.findAllByPostId({ postId });
   }
 
   @Get('comments/:id')
-  findOne(@Param('id') id: string) {
+  @ApiTags('comments')
+  @ApiOkResponse({ type: CommentDto })
+  findOne(@Param('id') id: string): Promise<CommentDto | null> {
     return this.commentsService.findOne({ id });
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiTags('comments')
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: CommentDto })
   @Patch('comments/:id')
   update(
     @Param('id') id: string,
     @Body() updateCommentDto: UpdateCommentDto,
     @Req() req: RequestWithUser,
-  ) {
+  ): Promise<CommentDto> {
     return this.commentsService.update(
       { ...updateCommentDto },
       { id },
@@ -61,8 +78,14 @@ export class CommentsController {
   }
 
   @UseGuards(JwtAuthGuard)
+  @ApiTags('comments')
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: CommentDto })
   @Delete('comments/:postId')
-  remove(@Param('postId') id: string, @Req() req: RequestWithUser) {
+  remove(
+    @Param('postId') id: string,
+    @Req() req: RequestWithUser,
+  ): Promise<CommentDto> {
     return this.commentsService.remove({ id }, req.user);
   }
 }
